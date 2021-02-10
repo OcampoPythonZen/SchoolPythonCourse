1ff5d4afa1e53b6d9e5048789c5e0f0475dff11a
OcampoPythonZen <eocampo1986@gmail.com>
2021-02-09 22:16:55 -0600

    pass generator finished

--- password_generator.py
+++ password_generator.py
@@ -4,24 +4,25 @@ import string
 
 class PasswordGenerator(Frame):
 
-    def __init__(self, master) -> None:
-        self.master = master
-        self.master.geometry("200X200")
+    def __init__(self, master=None):
+        super().__init__(master)
         self.characters = list(string.ascii_letters + string.punctuation)
         self.password = None
-        self.width= 8
+        self.len= 16
         self.pack()
         self.btnPassword()
         self.txtPassword()
         self.btnExit()
 
-    def _nuevoPassword(self):
-        pass
+    def _newPassword(self):
+        self.password = ''.join(random.choices(self.characters, k=self.len))
+        self.sv_txtPassword.set(self.password)
+        print(self.password)
 
     def btnPassword(self):
         self._btnPassword = Button(self)
         self._btnPassword['text'] = 'New Password'
-        self._btnPassword['command'] = self._nuevoPassword
+        self._btnPassword['command'] = self._newPassword
         self._btnPassword.pack(side='left')
 
     def txtPassword(self):
@@ -29,3 +30,17 @@ class PasswordGenerator(Frame):
         self._txtPassword = Entry(self, textvariable=self.sv_txtPassword) 
         self._txtPassword['width'] = 32
         self._txtPassword.pack(side="left")
+
+    def btnExit(self):
+        self._btnExit = Button(self)
+        self._btnExit['text']='Close'
+        self._btnExit['fg']='red'
+        self._btnExit['command']=self.master.destroy
+        self._btnExit.pack(side='right')
+
+if __name__ == '__main__':
+    root = Tk()
+    root.title('Password Generator by Ed.')
+    app = PasswordGenerator(master=root)
+    app.mainloop()
+    
